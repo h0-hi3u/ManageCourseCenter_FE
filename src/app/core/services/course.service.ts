18a3df4ff00683f8125043a5dc35e0923699e49a
h0-hi3u <hieuenglishabc@gmail.com>
2024-03-01 23:48:43 +0700

    course-detail-page connect api

--- src/app/core/services/course.service.ts
+++ src/app/core/services/course.service.ts
@@ -7,12 +7,18 @@ import { environment } from "src/environment/environment";
     providedIn: 'root'
 })
 export class CourseService {
-    private readonly urlGetAll = 'Course/get-all'
+    private readonly urlGetAll = 'Course/get-all';
+    private readonly urlDetail = 'Course/get-course-id';
     constructor(private http: HttpClient) {}
 
     public getAll() : Observable<ResponseDto> {
         return this.http.get<ResponseDto>(
             `${environment.urlApi}/${this.urlGetAll}`
+        )
+    }
+    public getDetailCourse(id: number): Observable<ResponseDto> {
+        return this.http.get<ResponseDto>(
+            `${environment.urlApi}/${this.urlDetail}?id=${id}`
         )
     }
 }
--- src/app/shared/home-page/home-page.component.ts
+++ src/app/shared/home-page/home-page.component.ts
@@ -18,6 +18,6 @@ export class HomePageComponent implements OnInit {
       })
   }
   public goToCourseDetail(id: number) {
-    this.router.navigate(['/course-detail']);
+    this.router.navigate([`/course-detail/${id}`]);
   }
 }
